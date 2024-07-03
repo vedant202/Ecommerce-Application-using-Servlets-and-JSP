867b0c3d9a4a124010ce090281e5b87b1bbb2231
vedant202 <vdhenge202@gmail.com>
2024-07-04 01:22:47 +0530

    Fiters and Sorting is done on admin product panel

--- src/main/webapp/WEB-INF/AdminHome.jsp
+++ src/main/webapp/WEB-INF/AdminHome.jsp
@@ -1,87 +1,192 @@
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1"
-    pageEncoding="ISO-8859-1"%>
+	pageEncoding="ISO-8859-1"%>
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Add New Product</title>
-    
-    <style>
-        .tableDiv{
-            overflow-x: auto;
-        }
-        .table{
-            width: 100%;
-            height: 100%;
-            border: 1px solid;
-            border-collapse: collapse;
-            text-align: left;
-            background-color: #212529;
-            color: white;
-            
-        }
-        
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Add New Product</title>
 
-        th, td{
-            border-bottom: 1px solid rgb(167, 164, 164);
-            vertical-align: middle;
-            padding: 13px;
-        }
-         tr:hover {
-            background-color: rgb(53, 52, 52);
-        }
-        td > a > button{
-            width: 80px;
-            height: 30px;
-            border-radius: 7px;
-            background-color: rgb(43, 226, 43);
-            color: white;
-            font-weight: 500;
-            cursor: pointer;
+<style>
+#search {
+	width: 17em;
+	height: 2.5em;
+}
 
-        }
-        td > button:hover {
-            background-color: green;
-        }
-    </style>
-    
-    <script type="text/javascript">
-    	var products = <%= request.getAttribute("products") %>;
-    	
-    </script>
-    
+.tableDiv {
+	overflow-x: auto;
+}
+
+.table {
+	width: 100%;
+	height: 100%;
+	border: 1px solid;
+	border-collapse: collapse;
+	text-align: left;
+	background-color: #212529;
+	color: white;
+}
+
+th, td {
+	border-bottom: 1px solid rgb(167, 164, 164);
+	vertical-align: middle;
+	padding: 13px;
+}
+
+tr:hover {
+	background-color: rgb(53, 52, 52);
+}
+
+td>a>button {
+	width: 80px;
+	height: 30px;
+	border-radius: 7px;
+	background-color: rgb(43, 226, 43);
+	color: white;
+	font-weight: 500;
+	cursor: pointer;
+}
+
+td>button:hover {
+	background-color: green;
+}
+
+.colsNames  th {
+	cursor: pointer;
+}
+
+#id:hover, #status:hover, #title:hover, #brand:hover, #cate:hover,
+	#stock:hover, #price:hover { .uparrow , .downarrow{ display:block;
+	
+}
+
+}
+.uparrow {
+	display: none;
+}
+
+.downarrow {
+	display: none;
+	transform: rotate(180deg);
+	text-align: end;
+}
+
+.cateList{
+	display: inline-block;
+}
+
+.cateList .anchor{
+	position: relative;
+	display: inline-block;
+	 padding: 5px 50px 5px 10px;
+  border: 1px solid #ccc;
+  background-color: white;
+  color: black;
+  cursor: pointer;
+}
+
+.cateList .anchor:after{
+	position: absolute;
+	content: "";
+	border-left: 2px solid black;
+	border-top: 2px solid black;
+	padding: 5px;
+	right: 10px;
+	top: 20%;
+	  transform: rotate(-135deg);
+	
+}
+
+.cateList .anchor:active:after {
+	right: 8px;
+	top: 21%;
+}
+.items {
+  padding: 2px;
+  display: none;
+  margin: 0;
+  border: 1px solid #ccc;
+  border-top: none;
+}
+.items{
+  list-style: none;
+      position: absolute;
+    width: 10.2em;
+    background: white;
+    color: black;
+    z-index: 10;
+}
+
+.cateList.visible .anchor {
+  color: #0094ff;
+}
+
+.cateList.visible .items {
+  display: block;
+}
+</style>
+
+<script type="text/javascript">
+	var products =
+<%=request.getAttribute("products")%>
+	;
+</script>
+
 </head>
 <body>
-    <div>
-    
-    	<button><a href="/FilterTuts/admin/add_product">Add Product</a></button>
-    	
-    	
-    	
-    	<div class="tableDiv">
-      <table border="0 0 1px solid" class="table">
-     
-        <thead>
-        <tr><th style="text-align: center;" colspan="8"><h1>ALL PRODUCTS</h1></th></tr>
-          <tr>
-            <th scope="col">ID</th>
-            <th scope="col">TITLE</th>
-            <th scope="col">BRAND</th>
-            <th scope="col">CATEGEORY</th>
-            <th>STATUS</th>
-            <th>STOCK</th>
-            <th>PRICE</th>
-            <th>Actions</th>
-          </tr>
-        </thead>
-        <tbody id="tableBody">
-            
-        </tbody>
-      </table>
-    </div>
-    </div>
-    
-    <script src="./js/AdminProduct.js" type="text/javascript"></script>
+	<div>
+
+		<button>
+			<a href="/FilterTuts/admin/add_product">Add Product</a>
+		</button>
+
+
+
+		<div class="tableDiv">
+			<table border="0 0 1px solid" class="table">
+
+				<thead>
+					<tr>
+						<th style="text-align: center;" colspan="5"><h1>ALL
+								PRODUCTS</h1></th>
+						<th>
+							<div class="cateList" id="cateList" tabindex="100">
+								<span class="anchor">Select Category</span>
+								<ul class="items" id="cateItems">
+									<li><input type="checkbox" />1</li>
+									<li><input type="checkbox" />Whiskey</li>
+									<li><input type="checkbox" />Grocery</li>
+								</ul>
+							</div>
+						</th>
+						<th colspan="2"><input type="search" id="search"
+							name="search" placeholder="Search"></th>
+					</tr>
+					<tr class="colsNames">
+						<th id="id" scope="col"><div class="uparrow">^</div>ID
+							<div class="downarrow">^</div></th>
+						<th id="title" scope="col"><div class="uparrow">^</div>TITLE
+							<div class="downarrow">^</div></th>
+						<th id="brand" scope="col"><div class="uparrow">^</div>BRAND
+							<div class="downarrow">^</div></th>
+						<th id="cate" scope="col"><div class="uparrow">^</div>CATEGEORY
+							<div class="downarrow">^</div></th>
+						<th id="status"><div class="uparrow">^</div>STATUS
+							<div class="downarrow">^</div></th>
+						<th id="stock"><div class="uparrow">^</div>STOCK
+							<div class="downarrow">^</div></th>
+						<th id="price"><div class="uparrow">^</div>PRICE
+							<div class="downarrow">^</div></th>
+						<th>Actions</th>
+					</tr>
+				</thead>
+				<tbody id="tableBody">
+
+				</tbody>
+			</table>
+		</div>
+	</div>
+
+	<script src="./js/AdminProduct.js" type="text/javascript"></script>
 </body>
 </html>
--- src/main/webapp/js/AdminProduct.js
+++ src/main/webapp/js/AdminProduct.js
@@ -1,5 +1,30 @@
 console.log(products)
 
+function cateClick(){
+    
+        console.log("cate clicked");
+        if(cateList.classList.contains('visible')){
+            console.log("visible contains")
+            cateList.classList.remove('visible');
+        }else{
+            console.log("visible not contains")
+            cateList.classList.add('visible')
+        }
+    
+}
+
+
+let cateList = document.getElementById("cateList");
+console.log(cateList.getElementsByClassName("anchor")[0])
+cateList.getElementsByClassName("anchor")[0].setAttribute("onclick","cateClick()")
+
+// Getting unique categoeries
+let uniqueCate = new Set(products.map(i=>i.category));
+console.log("uniqueCate",uniqueCate)
+let htmlCate="";
+uniqueCate.forEach((i)=>{htmlCate+=`<li><input type="checkbox" />${i}</li>`})
+document.getElementById('cateItems').innerHTML=htmlCate;
+
 let html ="";
 
 html+=products.map(i=>`<tr>
@@ -18,3 +43,155 @@ html+=products.map(i=>`<tr>
 )
 
 document.getElementById("tableBody").innerHTML = html.replaceAll(",","");
+
+let search = document.getElementById("search");
+search.addEventListener("input",(e)=>{
+    console.log(e.target.value);
+    let prods = products.filter(i=>(i.title.toLowerCase().includes(e.target.value?.toLowerCase()) || i?.brand?.toLowerCase().includes(e.target.value?.toLowerCase())))
+    let html =""
+    html += prods.map(i=>`<tr>
+        <td>${i?.id}</td>
+        <td>${i?.title}</td>
+        <td>${i?.brand}</td>
+        <td>${i?.category}</td>
+        <td>${i?.availabilityStatus}</td>
+        <td>${i?.stock}</td>
+        <td>${i?.price}</td>
+        <td>
+            <a href="http://localhost:8080/FilterTuts/admin/update/product?id=${i?.id}"><button class="actionButton">Update</button></a>
+            <a href="http://localhost:8080/FilterTuts/admin/delete/product?id=${i?.id}"><button class="actionButton">Delete</button></a>
+        </td>
+    </tr>`)
+
+    document.getElementById("tableBody").innerHTML = html.replaceAll(",","");
+
+})
+
+
+let headId = document.getElementById("id");
+let headTitle = document.getElementById("title")
+let headBrand = document.getElementById("brand")
+let headCate = document.getElementById("cate")
+let headStatus = document.getElementById("status")
+let headStock = document.getElementById("stock")
+let headPrice = document.getElementById("price")
+
+
+let sorted=products;
+let toggle = false;
+headId.addEventListener("click",(e)=>{
+    console.log("Id toggle")
+    
+    let html =""
+    if(toggle){
+        sorted = sorted.sort((a,b)=>a.id-b.id);
+        toggle=false;
+    }else{
+        sorted = sorted.sort((a,b)=>b.id-a.id);
+        toggle=true;
+    }
+    html += sorted.reverse().map(i=>`<tr>
+        <td>${i?.id}</td>
+        <td>${i?.title}</td>
+        <td>${i?.brand}</td>
+        <td>${i?.category}</td>
+        <td>${i?.availabilityStatus}</td>
+        <td>${i?.stock}</td>
+        <td>${i?.price}</td>
+        <td>
+            <a href="http://localhost:8080/FilterTuts/admin/update/product?id=${i?.id}"><button class="actionButton">Update</button></a>
+            <a href="http://localhost:8080/FilterTuts/admin/delete/product?id=${i?.id}"><button class="actionButton">Delete</button></a>
+        </td>
+    </tr>`)
+
+    document.getElementById("tableBody").innerHTML = html.replaceAll(",","");
+})
+toggle=false;
+headTitle.addEventListener("click",(e)=>{
+    console.log("title toggle")
+    if(toggle){
+        sorted = sorted.sort((a,b)=>b.title[0].codePointAt()-a.title[0].codePointAt());
+        toggle=false;
+
+    }else{
+        sorted = sorted.sort((a,b)=>a.title[0].codePointAt()-b.title[0].codePointAt());
+        toggle=true;
+    }
+    
+    
+    let html =""
+    html += sorted.map(i=>`<tr>
+        <td>${i?.id}</td>
+        <td>${i?.title}</td>
+        <td>${i?.brand}</td>
+        <td>${i?.category}</td>
+        <td>${i?.availabilityStatus}</td>
+        <td>${i?.stock}</td>
+        <td>${i?.price}</td>
+        <td>
+            <a href="http://localhost:8080/FilterTuts/admin/update/product?id=${i?.id}"><button class="actionButton">Update</button></a>
+            <a href="http://localhost:8080/FilterTuts/admin/delete/product?id=${i?.id}"><button class="actionButton">Delete</button></a>
+        </td>
+    </tr>`)
+    
+    document.getElementById("tableBody").innerHTML = html.replaceAll(",","");
+})
+toggle=false;
+
+headPrice.addEventListener("click",(e)=>{
+    console.log("price toggle")
+    
+    let html =""
+    if(toggle){
+        sorted = sorted.sort((a,b)=>a.price-b.price);
+        toggle=false;
+    }else{
+        sorted = sorted.sort((a,b)=>b.price-a.price);
+        toggle=true;
+    }
+    html += sorted.reverse().map(i=>`<tr>
+        <td>${i?.id}</td>
+        <td>${i?.title}</td>
+        <td>${i?.brand}</td>
+        <td>${i?.category}</td>
+        <td>${i?.availabilityStatus}</td>
+        <td>${i?.stock}</td>
+        <td>${i?.price}</td>
+        <td>
+            <a href="http://localhost:8080/FilterTuts/admin/update/product?id=${i?.id}"><button class="actionButton">Update</button></a>
+            <a href="http://localhost:8080/FilterTuts/admin/delete/product?id=${i?.id}"><button class="actionButton">Delete</button></a>
+        </td>
+    </tr>`)
+
+    document.getElementById("tableBody").innerHTML = html.replaceAll(",","");
+})
+
+toggle=false;
+
+headStock.addEventListener("click",(e)=>{
+    console.log("stock toggle")
+    
+    let html =""
+    if(toggle){
+        sorted = sorted.sort((a,b)=>a.stock-b.stock);
+        toggle=false;
+    }else{
+        sorted = sorted.sort((a,b)=>b.stock-a.stock);
+        toggle=true;
+    }
+    html += sorted.reverse().map(i=>`<tr>
+        <td>${i?.id}</td>
+        <td>${i?.title}</td>
+        <td>${i?.brand}</td>
+        <td>${i?.category}</td>
+        <td>${i?.availabilityStatus}</td>
+        <td>${i?.stock}</td>
+        <td>${i?.price}</td>
+        <td>
+            <a href="http://localhost:8080/FilterTuts/admin/update/product?id=${i?.id}"><button class="actionButton">Update</button></a>
+            <a href="http://localhost:8080/FilterTuts/admin/delete/product?id=${i?.id}"><button class="actionButton">Delete</button></a>
+        </td>
+    </tr>`)
+
+    document.getElementById("tableBody").innerHTML = html.replaceAll(",","");
+})
